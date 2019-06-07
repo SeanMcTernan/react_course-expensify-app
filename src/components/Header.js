07210e018568ea7c6712d0552585f07b191932ef
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2019-06-07 15:36:58 -0600

    Adding styles and borders to buttons and widgets

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@ import React from 'react'
 const Header = (props) => (
     
     <div className="header"> 
-
     <div className="container">
     <h1 className="header__title">{props.title}</h1>
     {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
