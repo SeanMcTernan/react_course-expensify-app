6c5c267f7f02d89386a0ab336ce1707218d39908
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2019-06-18 16:43:17 -0600

    Add new expensify app directory and boilerplate

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import IndecisionApp from './components/IndecisionApp'
 import './styles/styles.scss'
 import 'normalize.css/normalize.css'
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'))
 
 
