0e4b2aa2a40412ac0d9743626e84eee87aea0b9a
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2019-07-15 15:57:00 -0600

    Configure 'filer by date feature' with React-Dates and Moment

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,23 +1,48 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { setTextFilter } from '../actions/filters'
-import { sortByAmount } from '../actions/filters'
-import { sortByDate } from '../actions/filters'
+import { DateRangePicker } from 'react-dates'
+import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from '../actions/filters'
+
+class ExpenseListFilters extends React.Component {
+    state = {
+        calendarFocussed: null
+    }
+    onDatesChange = ({startDate, endDate}) => {
+        this.props.dispatch(setStartDate(startDate))
+        this.props.dispatch(setEndDate(endDate))
+    } 
+    onFocusChange = (calendarFocussed) => {
+        this.setState(() => ({ calendarFocussed }))
+    }
+
+    render() {
+    return (
+        <div>
+            <input type="text" value={this.props.filters.text} onChange={(e) => {
+                this.props.dispatch(setTextFilter(e.target.value))
+            }}/>
+    
+            <select value={this.props.filters.sortBy} onChange={(e) => { (e.target.value === "date" ? this.props.dispatch(sortByDate()) :  this.props.dispatch(sortByAmount())) 
+            }}>
+            <option value="date">Date</option>
+            <option value="amount">Amount</option>
+            </select>
+            <DateRangePicker
+                startDate={this.props.filters.startDate}
+                endDate={this.props.filters.endDate}
+                onDatesChange={this.onDatesChange}
+                focusedInput={this.state.calendarFocussed}
+                onFocusChange={this.onFocusChange}
+                showClearDates={true}
+                numberOfMonths={1}
+                isOutsideRange={() => false}
+            /> 
+        </div>
+    )
+}
+}
 
 
-const ExpenseListFilters = (props) => (
-    <div>
-        <input type="text" value={props.filters.text} onChange={(e) => {
-            props.dispatch(setTextFilter(e.target.value))
-        }}/>
-
-        <select value={props.filters.sortBy} onChange={(e) => { (e.target.value === "date" ? props.dispatch(sortByDate()) :  props.dispatch(sortByAmount())) 
-        }}>
-        <option value="date">Date</option>
-        <option value="amount">Amount</option>
-        </select>
-    </div>
-)
 
 const mapStateToProps = (state) => {
     return {
--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,9 +1,11 @@
+import moment from 'moment' 
+
 // Filter Reducer
 const filtersReducerDefaultState = {
     text: '',
-    sortBy: '',
-    startDate: undefined,
-    endDate: undefined
+    sortBy: 'date',
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month')
   };
   
 export const filtersReducer = (state = filtersReducerDefaultState, action) => {
