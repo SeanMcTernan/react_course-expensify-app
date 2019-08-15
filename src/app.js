bba900e011dc3ba00b439e76a52538fff5d64325
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2019-08-15 17:22:30 -0600

    Set up webpack to create a production file and better source maps

--- src/app.js
+++ src/app.js
@@ -16,21 +16,6 @@ import uuid from "uuid";
 
 const store = configureStore()
 
-
-store.dispatch(addExpense({ description: 'Gas bill', amount: 2, createdAt: 3 }))
-store.dispatch(addExpense({ description: 'Water bill', amount: 1, createdAt: 4 }))
-store.dispatch(addExpense({ description: 'Rent', amount: 3, createdAt: 5 }))
-
-
-// const state = store.getState()
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-// console.log(visibleExpenses);
-
-// store.dispatch(sortByDate())
-
-// store.dispatch(sortByAmount())
-
-
 const jsx = (
     <Provider store={store}>
     <AppRouter />
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
 
-
-module.exports = {
+module.exports = (env) => {
+ const isProduction = env === 'production'
+ return {
     entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
@@ -21,9 +22,10 @@ module.exports = {
             ]
         }], 
     },
-    devtool: 'cheap-module-source-map',
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public'),
         historyApiFallback: true
     }
-};
+}
+}
