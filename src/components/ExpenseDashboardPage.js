828cb84565ce855accdade18e8daf5c5608c01f4
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2019-08-28 16:44:01 -0600

    Push untracked case sensitive changes

---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ExpenseList from './ExpenseList'
+import ExpenListFilters from './ExpenseListFilters' 
+
+const ExpenseDashboardPage = () =>  (
+    <div>
+    <ExpenListFilters />
+    <ExpenseList />
+    </div>
+)
+
+export default ExpenseDashboardPage
