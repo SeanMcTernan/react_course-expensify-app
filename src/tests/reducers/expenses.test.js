07852367203e6a7b386159648496221a803e6fc3
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2019-09-16 19:41:22 -0600

    Setup actions to pull expenses from Firebase and Set them on the store

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,5 +1,6 @@
 import expensesReducer from '../../reducers/expenses'
 import expenses from '../fixtures/expenses'
+import moment from 'moment'
 
 test('Should set default state' , () => {
     const state = expensesReducer(undefined, { type: '@@INIT' })
@@ -87,4 +88,13 @@ test('Should not edit an expense in the array', () => {
     const state = expensesReducer(expenses, action)
     expect(state).toEqual([...expenses]
       )
+})
+
+test('Should set expenses', () => {
+    const action = {
+        type: "SET_EXPENSES",
+        expenses: expenses[1]
+    }
+    const state = expensesReducer(expenses, action)
+    expect(state).toEqual(expenses[1])
 })
